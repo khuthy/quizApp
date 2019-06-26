a7257ec8f8023536374ba99360cc13a87bb3eaca
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2019-06-26 04:39:25 -0700

    Update points.ts

--- src/providers/points/points.ts
+++ src/providers/points/points.ts
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PointsProvider {
   grandTotal: number = 0;
-  getData: any;
+  
   constructor(public http: HttpClient) {
     console.log('Hello PointsProvider Provider');
   }
@@ -18,8 +18,6 @@ export class PointsProvider {
 
   ScorePoints(val) {
       this.grandTotal += val;
-  }
-  getData(){
   }
   getScore(){
     return this.grandTotal;
